dec3600c608b73879fda09b2f5c08d8a72368397
Aniket Korwar <78900271+aniketh1@users.noreply.github.com>
2023-11-04 15:25:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,37 +10,58 @@ for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
     
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     audio[i].play();
+    var buttonValue = document.querySelectorAll("button")[i].innerHTML;
+    buttonPressed(buttonValue);
     
 });
     document.addEventListener("keydown",function(e){
     switch (e.key) {
         case "w":
             audio[0].play();
+            buttonPressed(e.key);
+            
             break;
         case "a":
             audio[1].play();
+            buttonPressed(e.key);
             break;
         case "s":
             audio[2].play();
+            buttonPressed(e.key);
             break;
         case "d":
             audio[3].play();
+            buttonPressed(e.key);
             break;
         case "j":
             audio[4].play();
+            buttonPressed(e.key);
             break;
         case "k":
             audio[5].play();
+            buttonPressed(e.key);
             break;
         case "l":
             audio[6].play();
+            buttonPressed(e.key);
             break;
     
         default:
             break;
     }
     
+    
 });
+}
+
+function buttonPressed(animation){
+    console.log("clicked")
+    var btn = document.querySelector("."+animation);
+    btn.classList.add("pressed");   
+    setTimeout(function(){
+        btn.classList.remove("pressed");
+    },200);
+    
 }
 
 // new Audio("./sounds/crash.mp3")
